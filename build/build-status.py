a40b7d23683fbc4bf51aafaf34ef0c56a1b70a52
applex250 <2834176064@qq.com>
2025-09-19 21:42:37 +0800

    fist commit

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/subdir/ns3.34-subdir-debug', 'build/scratch/ns3.34-test_channel-debug', 'build/scratch/ns3.34-OFSDN-debug', 'build/scratch/ns3.34-scratch-simulator-debug', 'build/scratch/ns3.34-och_test-debug', 'build/scratch/ns3.34-sdn-debug', 'build/scratch/ns3.34-first-debug', 'build/scratch/ns3.34-third-debug', 'build/scratch/ns3.34-ts2-debug', 'build/scratch/ns3.34-ts22-debug', 'build/scratch/ns3.34-cc2-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
--- openflow/waf.bat
+++
@@ -1,1 +0,0 @@
-@python3 -x waf %* & exit /b
